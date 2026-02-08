383879fe702ce50e2c52c54e23608678463b2c0f
Marina Cordeiro <cordeiromarina737@gmail.com>
2026-02-07 21:06:54 -0300

    adc de button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { words } from '../constants'
 import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 
 
 const Hero = () => {
@@ -13,8 +14,8 @@ const Hero = () => {
 
         <div className='hero-layout'>
             {/*LEFT: HERO CPNTENT */}
-            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-                <div className='flex flex-col gap-7'>
+            <header className='flex flex-col justify-center md:w-full w-screen md:px-10 px-5'>
+                <div className='flex flex-col gap-4'>
                     <div className='hero-text'>
                       <h1>Transformando
                          <span className='slide'>
@@ -49,6 +50,11 @@ const Hero = () => {
                 </div>
             </header>
             {/*RIGHT: 3D MODEL*/}
+            <figure>
+                <div className='hero-3d-layout'>
+                  <HeroExperience/>
+                </div>
+            </figure>
 
         </div>
      </section>
